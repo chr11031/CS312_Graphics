899f0d0945141a76e3a3c3c319f0431552b3efba
bjaffray <lithillia@Lithillia.local>
2019-01-11 10:48:07 -0700

    update 1

--- coursefunctions.h
+++ coursefunctions.h
@@ -167,49 +167,49 @@ void TestDrawTriangle(Buffer2D<PIXEL> & target)
         *************************************************/
         Vertex verts[3];
         Attributes attr[3];
-        verts[0] = {100, 362, 1, 1};
-        verts[1] = {150, 452, 1, 1};
-        verts[2] = {50, 452, 1, 1};
+        verts[0] = (Vertex){100, 362, 1, 1};
+        verts[1] = (Vertex){150, 452, 1, 1};
+        verts[2] = (Vertex){50, 452, 1, 1};
         PIXEL colors1[3] = {0xffff0000, 0xffff0000, 0xffff0000};
         // Your color code goes here for 'attr'
 
         DrawPrimitive(TRIANGLE, target, verts, attr);
 
-        verts[0] = {300, 402, 1, 1};
-        verts[1] = {250, 452, 1, 1};
-        verts[2] = {250, 362, 1, 1};
+        verts[0] = (Vertex){300, 402, 1, 1};
+        verts[1] = (Vertex){250, 452, 1, 1};
+        verts[2] = (Vertex){250, 362, 1, 1};
         PIXEL colors2[3] = {0xffff0000, 0xffff0000, 0xffff0000};
         // Your color code goes here for 'attr'
 
         DrawPrimitive(TRIANGLE, target, verts, attr);
 
-        verts[0] = {450, 362, 1, 1};
-        verts[1] = {450, 452, 1, 1};
-        verts[2] = {350, 402, 1, 1};
+        verts[0] = (Vertex){450, 362, 1, 1};
+        verts[1] = (Vertex){450, 452, 1, 1};
+        verts[2] = (Vertex){350, 402, 1, 1};
         PIXEL colors3[3] = {0xff00ff00, 0xff00ff00, 0xff00ff00};
         // Your color code goes here for 'attr'
 
         DrawPrimitive(TRIANGLE, target, verts, attr);
         
-        verts[0] = {110, 262, 1, 1};
-        verts[1] = {60, 162, 1, 1};
-        verts[2] = {150, 162, 1, 1};
+        verts[0] = (Vertex){110, 262, 1, 1};
+        verts[1] = (Vertex){60, 162, 1, 1};
+        verts[2] = (Vertex){150, 162, 1, 1};
         PIXEL colors4[3] = {0xff00ff00, 0xff00ff00, 0xff00ff00};
         // Your color code goes here for 'attr'
 
         DrawPrimitive(TRIANGLE, target, verts, attr);
 
-        verts[0] = {210, 252, 1, 1};
-        verts[1] = {260, 172, 1, 1};
-        verts[2] = {310, 202, 1, 1};
+        verts[0] = (Vertex){210, 252, 1, 1};
+        verts[1] = (Vertex){260, 172, 1, 1};
+        verts[2] = (Vertex){310, 202, 1, 1};
         PIXEL colors5[3] = {0xff00ff00, 0xff00ff00, 0xff00ff00};
         // Your color code goes here for 'attr'
 
         DrawPrimitive(TRIANGLE, target, verts, attr);
         
-        verts[0] = {370, 202, 1, 1};
-        verts[1] = {430, 162, 1, 1};
-        verts[2] = {470, 252, 1, 1};
+        verts[0] = (Vertex){370, 202, 1, 1};
+        verts[1] = (Vertex){430, 162, 1, 1};
+        verts[2] = (Vertex){470, 252, 1, 1};
         PIXEL colors6[3] = {0xff00ff00, 0xff00ff00, 0xff00ff00};
         // Your color code goes here for 'attr'
 
@@ -228,9 +228,9 @@ void TestDrawFragments(Buffer2D<PIXEL> & target)
         *************************************************/
         Vertex colorTriangle[3];
         Attributes colorAttributes[3];
-        colorTriangle[0] = {250, 112, 1, 1};
-        colorTriangle[1] = {450, 452, 1, 1};
-        colorTriangle[2] = {50, 452, 1, 1};
+        colorTriangle[0] = (Vertex){250, 112, 1, 1};
+        colorTriangle[1] = (Vertex){450, 452, 1, 1};
+        colorTriangle[2] = (Vertex){50, 452, 1, 1};
         PIXEL colors[3] = {0xffff0000, 0xff00ff00, 0xff0000ff}; // Or {{1.0,0.0,0.0}, {0.0,1.0,0.0}, {0.0,0.0,1.0}}
         // Your color code goes here for 'colorAttributes'
 
@@ -247,9 +247,9 @@ void TestDrawFragments(Buffer2D<PIXEL> & target)
         ****************************************************/
         Vertex imageTriangle[3];
         Attributes imageAttributes[3];
-        imageTriangle[0] = {425, 112, 1, 1};
-        imageTriangle[1] = {500, 252, 1, 1};
-        imageTriangle[2] = {350, 252, 1, 1};
+        imageTriangle[0] = (Vertex){425, 112, 1, 1};
+        imageTriangle[1] = (Vertex){500, 252, 1, 1};
+        imageTriangle[2] = (Vertex){350, 252, 1, 1};
         double coordinates[3][2] = { {1,0}, {1,1}, {0,1} };
         // Your texture coordinate code goes here for 'imageAttributes'
 
@@ -322,9 +322,9 @@ void TestVertexShader(Buffer2D<PIXEL> & target)
         *************************************************/
         Vertex colorTriangle[3];
         Attributes colorAttributes[3];
-        colorTriangle[0] = { 350, 112, 1, 1};
-        colorTriangle[1] = { 400, 200, 1, 1};
-        colorTriangle[2] = { 300, 200, 1, 1};
+        colorTriangle[0] = (Vertex){ 350, 112, 1, 1};
+        colorTriangle[1] = (Vertex){ 400, 200, 1, 1};
+        colorTriangle[2] = (Vertex){ 300, 200, 1, 1};
 
         PIXEL colors[3] = {0xffff0000, 0xff00ff00, 0xff0000ff};
         // Your code for 'colorAttributes' goes here
