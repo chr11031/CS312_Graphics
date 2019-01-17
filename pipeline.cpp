315725c910da5b27ce45e7d2562ab2ab5da36595
cynblkfox <curtis.wakley@gmail.com>
2019-01-16 19:14:23 -0700

    Implemented Triangle Drawing

--- pipeline.cpp
+++ pipeline.cpp
@@ -1,5 +1,6 @@
 #include "definitions.h"
 #include "coursefunctions.h"
+#include <cmath>
 
 /***********************************************
  * CLEAR_SCREEN
@@ -60,7 +61,8 @@ void processUserInputs(bool & running)
  ***************************************/
 void DrawPoint(Buffer2D<PIXEL> & target, Vertex* v, Attributes* attrs, Attributes * const uniforms, FragmentShader* const frag)
 {
-    // Your code goes here
+    // Set our pixel according to the attribute value
+    target[(int)v[0].y][(int)v[0].x] = attrs[0].color;
 }
 
 /****************************************
@@ -77,9 +79,31 @@ void DrawLine(Buffer2D<PIXEL> & target, Vertex* const triangle, Attributes* cons
  * Renders a triangle to the target buffer. Essential 
  * building block for most of drawing.
  ************************************************************/
-void DrawTriangle(Buffer2D<PIXEL> & target, Vertex* const triangle, Attributes* const attrs, Attributes* const uniforms, FragmentShader* const frag)
+void DrawTriangle(Buffer2D<PIXEL> & target, Vertex* const triangle, Attributes* const attrs, Attributes* const uniforms = NULL, FragmentShader* const frag = NULL)
 {
-    // Your code goes here
+    int maxX = fmax(triangle[0].x, fmax(triangle[1].x, triangle[2].x));
+    int minX = fmin(triangle[0].x, fmin(triangle[1].x, triangle[2].x));
+    int maxY = fmax(triangle[0].y, fmax(triangle[1].y, triangle[2].y));
+    int minY = fmin(triangle[0].y, fmin(triangle[1].y, triangle[2].y));
+
+    Vertex vs1 = {triangle[1].x - triangle[0].x, triangle[1].y - triangle[0].y, 1, 1};
+    Vertex vs2 = {triangle[2].x - triangle[0].x, triangle[2].y - triangle[0].y, 1, 1};
+
+    for (int y = minY; y <= maxY; y++)
+    {
+        for (int x = minX; x <= maxX; x++)
+        {
+            Vertex q = {x - triangle[0].x, y - triangle[0].y, 1, 1};
+
+            float s = ((q.x*vs2.y)-(vs2.x*q.y)) / ((vs1.x*vs2.y)-(vs2.x*vs1.y));
+            float t = ((vs1.x*q.y)-(q.x*vs1.y)) / ((vs1.x*vs2.y)-(vs2.x*vs1.y));
+
+            if ((s >= 0) && (t >= 0) && (s + t <= 1))
+            {
+                target[y][x] = attrs[0].color;
+            }
+        }
+    }
 }
 
 /**************************************************************
@@ -185,6 +209,8 @@ int main()
         clearScreen(frame);
 
         // Your code goes here
+        // GameOfLife(frame);
+        TestDrawTriangle(frame);
 
         // Push to the GPU
         SendFrame(GPU_OUTPUT, REN, FRAME_BUF);
