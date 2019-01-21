5d5a59cc14d071afecb0419973bf99ab0819535b
RBudd16005 <bud16005@byui.edu>
2019-01-21 12:22:05 -0700

    Ryan_Budd_Project02_Triangle_Rasterization

--- pipeline.cpp
+++ pipeline.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "definitions.h"
 #include "coursefunctions.h"
 
@@ -81,210 +80,36 @@ void DrawLine(Buffer2D<PIXEL> & target, Vertex* const triangle, Attributes* cons
  ************************************************************/
 void DrawTriangle(Buffer2D<PIXEL> & target, Vertex* const triangle, Attributes* const attrs, Attributes* const uniforms, FragmentShader* const frag)
 {
-    // Your code goes here
-}
+    //First we will find all the minimum and maximum X and Y points
+    //to create the bounding box.
+    int maxX = MAX3(triangle[0].x, triangle[1].x, triangle[2].x);
+    int maxY = MAX3(triangle[0].y, triangle[1].y, triangle[2].y);
+    int minX = MIN3(triangle[0].x, triangle[1].x, triangle[2].x);
+    int minY = MIN3(triangle[0].y, triangle[1].y, triangle[2].y);
 
-/**************************************************************
- * VERTEX_SHADER_EXECUTE_VERTICES
- * Executes the vertex shader on inputs, yielding transformed
- * outputs. 
- *************************************************************/
-void VertexShaderExecuteVertices(const VertexShader* vert, Vertex const inputVerts[], Attributes const inputAttrs[], const int& numIn, 
-                                 Attributes* const uniforms, Vertex transformedVerts[], Attributes transformedAttrs[])
-{
-    // Defaults to pass-through behavior
-    if(vert == NULL)
+    //Find the edges of (triangle[0], triangle[1]) and (triangle[0], triangle[2])
+    Vertex vs1 = {triangle[1].x - triangle[0].x, triangle[1].y - triangle[0].y, 1, 1};
+    Vertex vs2 = {triangle[2].x - triangle[0].x, triangle[2].y - triangle[0].y, 1, 1};
+    
+    //Loop throught the box around the triangle
+    for (int x = minX; x <= maxX; x++)
     {
-        for(int i = 0; i < numIn; i++)
+        for (int y = minY; y <= maxY; y++)
         {
-            transformedVerts[i] = inputVerts[i];
-            transformedAttrs[i] = inputAttrs[i];
+            Vertex w0 = {x - triangle[0].x, y - triangle[0].y, 1, 1};
+
+            //Every pixel is then calculated with the coordinates of the points
+            float w1 = ((w0.x * vs2.y) - (w0.y * vs2.x)) / ((vs1.x * vs2.y) - (vs1.y * vs2.x));
+            float w2 = ((vs1.x * w0.y) - (vs1.y * w0.x)) / ((vs1.x * vs2.y) - (vs1.y * vs2.x));
+
+            //If the point is found inside the triangle, draw it.
+            if ((w1 >= 0) && (w2 >= 0) && (w1 + w2 <= 1))
+            {
+                Vertex vert = {x, y, 1, 1};
+                DrawPrimitive(POINT, target, &vert, attrs);
+            }
         }
     }
-}
-
-/***************************************************************************
- * DRAW_PRIMITIVE
- * Processes the indicated PRIMITIVES type through pipeline stages of:
- *  1) Vertex Transformation
- *  2) Clipping
- *  3) Normalization
- *  4) ViewPort transform
- *  5) Rasterization & Fragment Shading
- **************************************************************************/
-void DrawPrimitive(PRIMITIVES prim, 
-                   Buffer2D<PIXEL>& target,
-                   const Vertex inputVerts[], 
-                   const Attributes inputAttrs[],
-                   Attributes* const uniforms,
-                   FragmentShader* const frag,                   
-                   VertexShader* const vert,
-                   Buffer2D<double>* zBuf)
-{
-    // Setup count for vertices & attributes
-    int numIn = 0;
-    switch(prim)
-    {
-        case POINT:
-            numIn = 1;
-            break;
-        case LINE:
-            numIn = 2;
-            break;
-        case TRIANGLE:
-            numIn = 3;
-            break;
-    }
-
-    // Vertex shader 
-    Vertex transformedVerts[MAX_VERTICES];
-    Attributes transformedAttrs[MAX_VERTICES];
-    VertexShaderExecuteVertices(vert, inputVerts, inputAttrs, numIn, uniforms, transformedVerts, transformedAttrs);
-
-    // Vertex Interpolation & Fragment Drawing
-    switch(prim)
-    {
-        case POINT:
-            DrawPoint(target, transformedVerts, transformedAttrs, uniforms, frag);
-            break;
-        case LINE:
-            DrawLine(target, transformedVerts, transformedAttrs, uniforms, frag);
-            break;
-        case TRIANGLE:
-            DrawTriangle(target, transformedVerts, transformedAttrs, uniforms, frag);
-    }
-}
-
-/*************************************************************
- * MAIN:
- * Main game loop, initialization, memory management
- ************************************************************/
-int main()
-{
-    // -----------------------DATA TYPES----------------------
-    SDL_Window* WIN;               // Our Window
-    SDL_Renderer* REN;             // Interfaces CPU with GPU
-    SDL_Texture* GPU_OUTPUT;       // GPU buffer image (GPU Memory)
-    SDL_Surface* FRAME_BUF;        // CPU buffer image (Main Memory) 
-
-    // ------------------------INITIALIZATION-------------------
-    SDL_Init(SDL_INIT_EVERYTHING);
-    WIN = SDL_CreateWindow(WINDOW_NAME, 200, 200, S_WIDTH, S_HEIGHT, 0);
-    REN = SDL_CreateRenderer(WIN, -1, SDL_RENDERER_SOFTWARE);
-    FRAME_BUF = SDL_CreateRGBSurface(0, S_WIDTH, S_HEIGHT, 32, 0x000000ff, 0x0000ff00, 0x00ff0000, 0xff000000);
-    FRAME_BUF = SDL_ConvertSurface(SDL_GetWindowSurface(WIN), SDL_GetWindowSurface(WIN)->format, 0);
-    GPU_OUTPUT = SDL_CreateTextureFromSurface(REN, FRAME_BUF);
-    BufferImage frame(FRAME_BUF);
-
-    // Draw loop 
-    bool running = true;
-    while(running) 
-    {           
-        // Handle user inputs
-        //processUserInputs(running);
-
-        // Refresh Screen
-        //clearScreen(frame);
-
-        // Your code goes here
-        GameOfLife(frame);
-
-        // Push to the GPU
-        SendFrame(GPU_OUTPUT, REN, FRAME_BUF);
-    }
-
-    // Cleanup
-    SDL_FreeSurface(FRAME_BUF);
-    SDL_DestroyTexture(GPU_OUTPUT);
-    SDL_DestroyRenderer(REN);
-    SDL_DestroyWindow(WIN);
-    SDL_Quit();
-    return 0;
-}
-=======
-#include "definitions.h"
-#include "coursefunctions.h"
-
-/***********************************************
- * CLEAR_SCREEN
- * Sets the screen to the indicated color value.
- **********************************************/
-void clearScreen(Buffer2D<PIXEL> & frame, PIXEL color = 0xff000000)
-{
-    int h = frame.height();
-    int w = frame.width();
-    for(int y = 0; y < h; y++)
-    {
-        for(int x = 0; x < w; x++)
-        {
-            frame[y][x] = color;
-        }
-    }
-}
-
-/************************************************************
- * UPDATE_SCREEN
- * Blits pixels from RAM to VRAM for rendering.
- ***********************************************************/
-void SendFrame(SDL_Texture* GPU_OUTPUT, SDL_Renderer * ren, SDL_Surface* frameBuf) 
-{
-    SDL_UpdateTexture(GPU_OUTPUT, NULL, frameBuf->pixels, frameBuf->pitch);
-    SDL_RenderClear(ren);
-    SDL_RenderCopy(ren, GPU_OUTPUT, NULL, NULL);
-    SDL_RenderPresent(ren);
-}
-
-/*************************************************************
- * POLL_CONTROLS
- * Updates the state of the application based on:
- * keyboard, mouse, touch screen, gamepad inputs. 
- ************************************************************/
-void processUserInputs(bool & running)
-{
-    SDL_Event e;
-    int mouseX;
-    int mouseY;
-    while(SDL_PollEvent(&e)) 
-    {
-        if(e.type == SDL_QUIT) 
-        {
-            running = false;
-        }
-        if(e.key.keysym.sym == 'q' && e.type == SDL_KEYDOWN) 
-        {
-            running = false;
-        }
-    }
-}
-
-/****************************************
- * DRAW_POINT
- * Renders a point to the screen with the
- * appropriate coloring.
- ***************************************/
-void DrawPoint(Buffer2D<PIXEL> & target, Vertex* v, Attributes* attrs, Attributes * const uniforms, FragmentShader* const frag)
-{
-    // Your code goes here
-    target[(int)v[0].y][(int)v[0].x] = attrs[0].color;
-}
-
-/****************************************
- * DRAW_TRIANGLE
- * Renders a line to the screen.
- ***************************************/
-void DrawLine(Buffer2D<PIXEL> & target, Vertex* const triangle, Attributes* const attrs, Attributes* const uniforms, FragmentShader* const frag)
-{
-    // Your code goes here
-}
-
-/*************************************************************
- * DRAW_TRIANGLE
- * Renders a triangle to the target buffer. Essential 
- * building block for most of drawing.
- ************************************************************/
-void DrawTriangle(Buffer2D<PIXEL> & target, Vertex* const triangle, Attributes* const attrs, Attributes* const uniforms, FragmentShader* const frag)
-{
-    // Your code goes here
 }
 
 /**************************************************************
@@ -390,7 +215,7 @@ int main()
         clearScreen(frame);
 
         // Your code goes here
-        TestDrawPixel(frame);
+        TestDrawTriangle(frame);
 
         // Push to the GPU
         SendFrame(GPU_OUTPUT, REN, FRAME_BUF);
@@ -404,4 +229,3 @@ int main()
     SDL_Quit();
     return 0;
 }
->>>>>>> 32cc465e2030e0b991f4969ddccb8dd9f688816b
