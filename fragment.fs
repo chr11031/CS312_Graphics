e6cd38b8435783f19c3c6f7369d2fa8272bd35ae
Zach Newell <new14008@byui.edu>
2019-04-01 10:31:26 -0600

    open gl demo

---
+++ fragment.fs
@@ -0,0 +1,15 @@
+uniform sampler2D u_Texture;
+uniform float u_Threshold;
+
+varying vec2 v_UV;
+
+void main() 
+{
+    vec4 sample = texture2D(u_Texture, v_UV);    
+    if(sample.r > u_Threshold)
+    {
+        discard;
+    }
+
+    gl_FragColor = sample;
+}
