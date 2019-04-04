da73989403cfd82b2fea6151bcddf755999211ba
Brooke <nel13012@byui.edu>
2019-04-03 19:24:00 -0600

    Actual stuff for commit

--- phongFrag.fs
+++ phongFrag.fs
@@ -2,11 +2,11 @@ uniform sampler2D u_Texture;
 uniform float u_Threshold;
 uniform vec3 u_Camera;
 
+//Material For Ambient, Diffuse, Specular, and Shine
 uniform vec3 u_MaterialAmbient;
 uniform vec3 u_MaterialDiffuse;
 uniform vec3 u_MaterialSpecular;
 uniform vec3 u_MaterialShine;
-
 
 varying vec3 v_Position;
 varying vec2 v_UV;
@@ -15,6 +15,7 @@ varying vec3 v_Normal;
 void main()
 {
 
+	//Lighting
 	vec3 lightColor = vec3(1, 1, 1);
 	vec3 lightPos = vec3(10, 10, 10);
 
@@ -35,6 +36,7 @@ void main()
 	float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
     vec3 specular = u_MaterialSpecular * spec * lightColor;  
     
+	//Fragment Shader
     gl_FragColor = vec4((ambient + diffuse + specular), 1.0) * sample;
 
 }
