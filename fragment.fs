32ffbe681a8722020985338238667ca680fb8cc1
Zach Newell <new14008@byui.edu>
2019-04-03 20:14:11 -0600

    phong shading

--- fragment.fs
+++ fragment.fs
@@ -1,15 +1,31 @@
 uniform sampler2D u_Texture;
 uniform float u_Threshold;
+uniform vec3 u_Light;
+uniform vec3 u_LightPosition;
+uniform vec3 u_Ambient;
+uniform vec3 u_Diffuse;
+uniform vec3 u_Specular;
+uniform vec3 u_Camera;
+uniform float u_Strength;
 
 varying vec2 v_UV;
+varying vec3 v_Normal;
+varying vec4 v_Position;
 
 void main() 
 {
     vec4 sample = texture2D(u_Texture, v_UV);    
-    if(sample.r > u_Threshold)
-    {
-        discard;
-    }
+    vec3 ambient = u_Ambient * u_Light;
 
-    gl_FragColor = sample;
+    vec3 diffuse = max(dot(v_Normal, normalize(u_LightPosition)), 0.0) * (u_Diffuse * u_Light);
+
+    vec3 viewDirection = normalize(u_Camera.xyz - v_Position.xyz);
+    vec3 reflectDirection = normalize(reflect(-u_LightPosition, v_Normal));
+
+    float specularFloat = pow(max(dot(viewDirection, reflectDirection), 0.0), u_Strength);
+    vec3 specular = specularFloat * (u_Specular * u_Light);
+
+    vec4 result = vec4((ambient + diffuse +specular), 1.0) * sample; 
+
+    gl_FragColor = result;
 }
--- vertex.vs
+++ vertex.vs
@@ -1,11 +1,19 @@
 uniform mat4 u_Matrix;
 attribute vec3 a_Position;
 attribute vec2 a_UV;
+attribute vec3 a_Normal;
 
 varying vec2 v_UV;
+varying vec3 v_Normal;
+varying vec4 v_Position;
 
 void main()
 {
     v_UV = a_UV;
-    gl_Position = u_Matrix * vec4(a_Position, 1.0);
+    vec4 normal = vec4(a_Normal, 0.0);
+    v_Position = u_Matrix * vec4(a_Position, 1.0);
+
+    gl_Position = v_Position;
+
+    v_Normal.xyz = normalize(u_Matrix * normal).xyz; 
 }
