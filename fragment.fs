2d688cc12bb216b09de2511a3fc1ac17eb52c26d
John Miller <jdmdogsled@gmail.com>
2019-04-03 21:50:08 -0600

    Phong Shader (broken)
    Something is broken with my vertex shader, however the shaders should be working. I am hoping to fix it by tomorrow but I am turning in what I can now until I solve it.

---
+++ fragment.fs
@@ -0,0 +1,14 @@
+uniform sampler2D u_Texture;
+uniform float u_Threshold;
+
+varying vec2 v_UV;
+
+void main()
+{
+    vec4 sample = texture2D(u_Texture, v_UV);    
+    if(sample.r > u_Threshold)
+    {
+        discard;
+    }
+    gl_FragColor = sample;//(1.0,1.0,1.0,0.0);
+}
