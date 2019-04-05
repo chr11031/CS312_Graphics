482cf0775fb976765fa9b33b68a20c9c66e02738
calliday <calhen100@gmail.com>
2019-04-04 22:43:34 -0600

    Caleb_Hensley_Phong_BranchFix

--- fragment.fs
+++ fragment.fs
@@ -1,39 +1,46 @@
 uniform sampler2D u_Texture;
 uniform float u_Threshold;
 
+// hard coding light
 uniform vec3 light = {0, 100, -50};
 uniform vec3 lightColor = {1.0, 1.0, 1.0};
-uniform vec3 u_Cam;
 
+// inserted uniforms
 uniform float u_Ns;
 uniform vec3 u_Ka;
 uniform vec3 u_Kd;
 uniform vec3 u_Ks;
+uniform vec3 u_Camera;
 
 varying vec3 v_Normal;
 varying vec2 v_UV;
-varying vec3 frag_pos;
+varying vec3 fragPos;
 
 void main()
 {
     vec4 sample = texture2D(u_Texture, v_UV);
 
+    // if (sample.r > u_Threshold)
+    // {
+    //     discard;
+    // }
+    
     // ambient lighting
     vec3 ambient = u_Ka * lightColor;
-    
+
     // diffuse lighting
     vec3 norm = normalize(v_Normal);
-    vec3 lightDir = normalize(light - frag_pos);
-    float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = diff * lightColor * u_Kd;
+    vec3 lightDir = normalize(light - fragPos);
+    vec3 diffuse = max(dot(norm, lightDir), 0.0) * u_Kd * lightColor;
 
     // specular lighting
-    vec3 viewDir = normalize(u_Cam - frag_pos);
-    vec3 reflectDir = reflect(-lightDir, norm);  
+    vec3 viewDir = normalize(u_Camera - fragPos);
+    vec3 reflectDir = reflect(-lightDir, norm);
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), u_Ns);
-    vec3 specular = u_Ks * spec * lightColor;  
+    vec3 specular = u_Ks * spec * lightColor;
 
-    // the total phong shading
-    vec4 total = vec4((ambient + diffuse + specular), 1.0) * sample;
+    // adding them all together
+    vec4 total = vec4((diffuse + ambient + specular), 1.0) * sample;
+
     gl_FragColor = total;
-}
+} 
